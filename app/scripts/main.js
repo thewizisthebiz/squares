392b8b65df6ce5f5920bdfea902db4abde83ceaa
thewizisthebiz <joshua.kruszynski@gmail.com>
2017-08-26 17:49:29 -0400

    working draggabilly on child notes. minor tweaks to sizing. linked board title for easy reload.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,12 +4,19 @@ var mySwiper = new Swiper ('.swiper-container', {
 	// Optional parameters
 	slidesPerView: 2,
 	centeredSlides: true,
-	spaceBetween: 30,
+	spaceBetween: 10,
+	grabCursor: true,
 });
 
 var simpleBoard = new DrawingBoard.Board('simple-board', {
 	controls: false,
 	webStorage: false,
 	background: false,
+	size: 4,
 });
 
+var $draggable = $('.c_note--Child').draggabilly({
+  axis: 'y',
+  handle: '.note_handle',
+  containment: '.c_stack',
+});
